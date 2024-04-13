cb37348b698ca07fe930fad118e2489351e62f0e
colonia-duck <tomclarke2006@hotmail.com>
2024-04-13 20:08:41 +0100

    updated to use serde and formated the json

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,56 @@
-use reqwest::Error; // Import Error type from reqwest
-use std::io::{self};
+use std::io;
 
-async fn fetch_data(param_value: &str) -> Result<(), Error> {
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Debug,Serialize, Deserialize)]
+struct EdsmInfo {
+    #[serde(rename = "camalCase")]
+    name: String,
+    information: Vec<Info>,
+}
+#[derive(Debug,Serialize, Deserialize)]
+struct Info {
+    allegiance: String,
+    government: String,
+    faction: String,
+    population: i64,
+    security: String,
+    economy: String,
+    #[serde(rename = "secondEconomy")]
+    second_economy: String,
+    service: String,
+    #[serde(rename = "primaryStar")]
+    primary_star: String,
+    name: String,
+    #[serde(rename = "isScoopable")]
+    is_scoopable: bool
+}
+async fn fetch_data(param_value: &str) -> Result<(), reqwest::Error> {
     let url = "https://www.edsm.net/api-v1/system";
     
     let mut params = std::collections::HashMap::new();
     params.insert("systemName", param_value);
     params.insert("showInformation","1");
     params.insert("showPrimaryStar","1");
-    
    
 
-    let client = reqwest::Client::new();
-    let response = client.get(url)
+    let response: serde_json::Value = reqwest::Client::new()
+        .get(url)
         .query(&params)
         .send()
+        .await?
+        .json()
         .await?;
 
 
-    // Check if the request was successful (status code 200)
-    if response.status().is_success() {
-        // Read the response body as a string
-        let body = response.text().await?;
-        println!("Response body: {}", body);
-    } else {
-        println!("Error: {}", response.status());
-    }
+    println!("{:#?}", response);
 
     Ok(())
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), reqwest::Error> {
 
     println!("please enter the name of the system you want:");
 
